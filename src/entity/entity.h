e03ff3dea6f3a8ec4fc7b665eefe3db09f4cc9c6
wlle <qianshou0@gmail.com>
2025-05-11 21:17:46 +0800

    不小心将写了几个小时的代码都删了🥲

---
+++ src/entity/entity.h
@@ -0,0 +1,26 @@
+// #include <main/attack.hpp>
+// #include <main/bullet.h>
+// #include <main/entity.h>
+// #include <main/game.h>
+// #include <main/plant.h>
+// #include <main/tools.h>
+// #include <main/zombie.h>
+
+#pragma once
+
+namespace demo {
+
+enum class EntityType
+{
+    None = 0,
+    Plant,
+    Zombie,
+    Tool
+};
+
+class Entity
+{
+protected:
+};
+
+} // namespace demo
